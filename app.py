24847c67749ef929f54a589c9024299d299f7fe2
a06505302-cpu <a06505302@gmail.com>
2026-03-24 15:14:53 +0200

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-import os
 from flask import Flask, request, jsonify
+import random
+import time
 
 app = Flask(__name__)
 
 @app.route('/check')
 def check():
-    data = request.args.get('data')
-    return jsonify({"status": "ok", "input": data})
+    card = request.args.get('card')
 
-port = int(os.environ.get("PORT", 5500))
-app.run(host="0.0.0.0", port=port)
+    # تجربة (تقدر تغيرها لاحقًا)
+    result = random.choice(["approved", "live", "declined"])
+
+    return jsonify({
+        "card": card,
+        "result": result,
+        "message": f"{result.upper()} - processed",
+        "time": int(time.time())
+    })
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
