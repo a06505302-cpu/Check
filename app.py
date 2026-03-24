255e5731d894b1fee54579c583f912ec3e820a6e
a06505302-cpu <a06505302@gmail.com>
2026-03-24 14:39:10 +0200

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/check', methods=['GET'])
+@app.route('/check')
 def check():
     data = request.args.get('data')
-    return jsonify({
-        "status": "ok",
-        "input": data
-    })
+    return jsonify({"status": "ok", "input": data})
 
-app.run(host="0.0.0.0", port=5500)
+port = int(os.environ.get("PORT", 5500))
+app.run(host="0.0.0.0", port=port)
